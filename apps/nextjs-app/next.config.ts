0a06c5ee36989a659d1c90a5ffdfe70828673ed5
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-02-28 13:32:14 +0200

    remove nextConfig.images.remotePatterns

--- apps/nextjs-app/next.config.ts
+++ apps/nextjs-app/next.config.ts
@@ -2,33 +2,15 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable unicorn/prefer-module */
-/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 /* eslint-disable @typescript-eslint/require-await */
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 
 const {composePlugins, withNx} = require("@nx/next");
 const {withPayload} = require("@payloadcms/next/withPayload");
-
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */,
-        ].map((item) => {
-            const url = new URL(item);
-
-            return {
-                hostname: url.hostname,
-                protocol: url.protocol.replace(":", ""),
-            };
-        }),
-    },
     nx: {
         // Set this to true if you would like to use SVGR
         // See: https://github.com/gregberge/svgr
